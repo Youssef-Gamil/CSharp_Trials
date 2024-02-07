2e674717af9a3c3f08bb68dcee42fb3c72cea863
Youssef Gamil Kamel Hasaballah <ygamil@ezzsteel.com>
2024-02-07 20:03:03 +0200

    New MVC Project "School" - model, controller and index view created.

---
+++ MVCWebApplications/SchoolProject_MVC/Controllers/TeacherController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolProject_MVC.Models;
+
+namespace SchoolProject_MVC.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private static List<Teacher> Teachers = new List<Teacher>()
+        {
+            new Teacher {Id = 1, Name ="Youssef Gamil", BlogUrl="http://youssefgamil.com", DOB=DateTime.Now.AddYears(-28), Email="youssef@yahoo.com", NationalId="12345678912345", Password="ABCDF123", PhoneNumber="01001234567", Salary=5000}
+        };
+        public IActionResult Index()
+        {
+            return View(Teachers);
+        }
+    }
+}
---
+++ MVCWebApplications/SchoolProject_MVC/Models/Teacher.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolProject_MVC.Models
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        [Required]//for Validation
+        public string Name { get; set; }
+        [Url]//for UI Behaviour and validation (check it is an absolute url or not)
+        [Display(Name ="Blog URL")]//for UI Behaviour
+        public string BlogUrl { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [RegularExpression("[A-Fa-f1-6Yy]{6,}")]//for Validation
+        [DataType(DataType.Password)]//for UI Behaviour in edit mode
+        public string Password { get; set; }
+        [Required]//for Validation
+        [RegularExpression("0-9{14}")]//for Validation
+        [Display(Name="National ID")]//for UI Behaviour
+        public string NationalId { get; set; }
+        [Display(Name ="Date of Birth")]//for UI Behaviour
+        public DateTime DOB { get; set; }
+        [Phone]//for UI Behaviour & Validation
+        [Display(Name = "Phone Number")]//for UI Behaviour
+        public string PhoneNumber { get; set; }
+        [Range(500,50000)]//For server side validation
+        [DataType(DataType.Currency)]//for UI Behaviour
+        public Double Salary { get; set; }
+    }
+}
